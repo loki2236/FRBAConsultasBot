4d974e8e3835e885abdaa759b3ec976c6791c66e
Loki2236 <28462447+loki2236@users.noreply.github.com>
2021-03-25 10:35:29 -0300

    Added husky pre-commit + mocha test. Updated dependencies

---
+++ test/test.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+let assert = require('assert');
+
+suite('Testing Suite', function() {
+  suite('Config().feature', function() {
+    test('Should be an object that has a key for 0 (Default)', function() {
+      assert.ok(require('../utils/config').features['0']);
+    });
+    test('Should be an object with the config for each group', function() {
+      let obj = {
+        '-1001262375149': {
+          enableDeleteSystemMessages: false,
+          enableLinks: false,
+          enableValidateUsers: false,
+          enableRotate: false,
+          enableGoogle: false,
+          enableNuke: true,
+          enableExcel: true,
+        },
+        // defaults
+        '0': {
+          enableDeleteSystemMessages: false,
+          enableLinks: false,
+          enableValidateUsers: false,
+          enableRotate: false,
+          enableGoogle: false,
+          enableNuke: true,
+          enableExcel: true,
+        },
+      };
+      assert.deepStrictEqual(require('../utils/config').features, obj);
+    });
+
+  });
+});
